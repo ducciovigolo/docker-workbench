92427d50f758ddb2661e33a8603981624248d8ee
ducciovigolo <duccio.vigolo@gmail.com>
2024-03-02 18:22:13 +0100

    --

--- main/images-gallery/api/main.py
+++ main/images-gallery/api/main.py
@@ -12,7 +12,7 @@ DEBUG = bool(os.environ.get("DEBUG", True))
 
 if not UNSPLASH_KEY:
     raise EnvironmentError(
-        "Please create .env.local file and insert there UNSPLASH_KEY"
+        "Please provide UNSPLASH_KEY, for instance creating .env.local file and inserting there UNSPLASH_KEY"
     )
 
 app = Flask(__name__)
@@ -26,7 +26,7 @@ def new_image():
     word = request.args.get("query")
     headers = {"Accept-Version": "v1", "Authorization": "Client-ID " + UNSPLASH_KEY}
     params = {"query": word}
-    response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
+    response = requests.get(url=UNSPLASH_URL, headers=headers, params=params, verify=False)
 
     data = response.json()
     return data
